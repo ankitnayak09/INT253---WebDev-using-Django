38bb2d85c9589c376541f453bd1170aa6408cc70
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2024-01-10 15:40:01 +0530

    Basic Python

---
+++ test.py
@@ -0,0 +1,58 @@
+# variable
+student = "rahul"
+print(student)
+
+# DataType
+var1 = 30
+print(var1)
+print(type(var1))
+
+# operator
+a = 10
+b = 12
+
+# arithmetic
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+
+# relational
+print(a < b)
+print(a > b)
+print(a == b)
+print(a != b)
+
+# conditional
+print(a and b)
+print(a or b)
+
+# character
+char1 = 'a'
+# Strings
+str1 = "Hello World"
+# multiline strings
+str3 = '''
+Hello World
+This is a multiline String
+'''
+print(char1)
+print(str1)
+print(str3)
+
+# Tuple => order collection of elements
+# Tuple is immutable
+tuple1 = (2,'rahul',True)
+print(tuple1)
+print(type(tuple1))
+# tuple1[0] = 5
+# Above Line Gives Error Coz Tuples are immutable
+
+# List []
+list1 = []
+
+# Dictionary
+
+
+
